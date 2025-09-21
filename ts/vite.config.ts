7c1444922f021ffce70e4bb7e8bccabf6ecbfac2
Rmin-cell <momeni.armin82@gmail.com>
2025-09-21 22:30:08 +0330

    Change the color pallette of the project to vscode dark theme

--- ts/vite.config.ts
+++ ts/vite.config.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   root: ".",
   server: {
     port: 5173
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  esbuild: {
+    jsx: 'automatic'
   }
 });
 
